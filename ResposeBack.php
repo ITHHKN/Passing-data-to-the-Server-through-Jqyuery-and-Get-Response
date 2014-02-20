b55903fcc3394542c9bbb5ecb94b29514bbff361
Hasnain Haider Khan <it_hhkn@yahoo.com>
2014-02-20 12:41:18 +0500

    Update ResposeBack.php

--- ResposeBack.php
+++ ResposeBack.php
@@ -1,12 +1,26 @@
+    <?php
 
-<?php
+    $username = $_REQUEST['name'];
+    $pass     = $_REQUEST['pass'];
+    $email    = $_REQUEST['email'];
 
-$username = $_REQUEST['name'];
-$pass     = $_REQUEST['pass'];
-$email    = $_REQUEST['email'];
 
-echo $pass;
-echo $username;
-echo $email;
+    if($username ==""){
 
-?> 
+        echo "Please Enter the UserName";
+    }
+    elseif($pass==""){
+
+        echo "Please Enter The Pass";
+    }
+
+    elseif($email==""){
+
+        echo "Please Enter the Email";
+    }
+    else{
+
+        echo "All is Correct Goes On..!!!";
+    }
+
+    ?> 
